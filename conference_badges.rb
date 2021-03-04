a0779cee982bd0955f45e5105139ecd98d67d0da
Isaiah Montoya <idmontoya@yahoo.com>
2021-03-03 15:20:17 -0900

    commit tree 4781d7b034509dc29a0ff85d295c48b28e2e68ba

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Chrales", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
+    attendees.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |name|
+    puts name
+    end
+    assign_rooms(attendees).each do |assignments|
+    puts assignments
+    end
+end
